7ef8f535678ce5abd75cc4c7942899323504ef96
DenysZakharovGH <47418226+DenysZakharovGH@users.noreply.github.com>
2019-06-25 16:32:05 +0300

    Update settings.py

--- Django/settings.py
+++ Django/settings.py
@@ -21,10 +21,10 @@ from .Keys import SECRET_KEY
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
