71dfa2b7c97e3f162b60dc986dee81b57bfaf853
DenysZakharovGH <47418226+DenysZakharovGH@users.noreply.github.com>
2019-06-26 09:04:38 +0300

    Update settings.py

--- Django/settings.py
+++ Django/settings.py
@@ -17,11 +17,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-from .Keys import SECRET_KEY
+
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '№№№№№№№№№№№№№№№№№№№№№№№№№№№№')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
